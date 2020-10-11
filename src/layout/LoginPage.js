82ce3972c08a65c717974e22fd2877a24c86a02a
santiago lozano <santiagolozal@outlook.com>
2020-10-11 16:14:28 -0500

    inicio de sesión

--- src/layout/LoginPage.js
+++ src/layout/LoginPage.js
@@ -1,29 +1,69 @@
 import React, { Fragment, useState } from "react";
+import axios from "axios";
+import clienteAxios from "../config/axios";
+import { useHistory } from "react-router-dom";
 
 const LoginPage = () => {
+  let history = useHistory();
+  const [user, setUser] = useState({
+    email:'',
+    password:''
+  })
+  const [error, actualizarError] = useState(false);
+  const {email, password} = user
+  const actualizarState = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const submitLogin = async (e) => {
+    e.preventDefault();
+    console.log(clienteAxios);
+    // Validar
+    if ( email.trim() === "" || password.trim() === "") {
+      actualizarError(true);
+      return;
+    }
+    actualizarError(false);
+    let json = JSON.stringify(user);
+    let params = "json=" + json;
+    axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+    clienteAxios.post("/user/login", params)
+      .then(function (response) {
+        console.log(response);
+        history.push('/productos')
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
   return (
     <Fragment>
       
       <div className="card margincard">
         <h2 className="title">Inicia sesión</h2>
         <div className="card-body">
-          <form className="form">
+          <form className="form" onSubmit={submitLogin}>
             <div className="form-group">
               <label for="exampleInputEmail1">Email </label>
               <input
+                name="email"
                 type="email"
                 className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
+                onChange={actualizarState}
+                value={email}
               />
               
             </div>
             <div className="form-group">
               <label for="exampleInputPassword1">Contraseña</label>
               <input
+                name="password"
                 type="password"
                 className="form-control"
-                id="exampleInputPassword1"
+                onChange={actualizarState}
+                value={password}
               />
             </div>
             <button type="submit" className="btn btn-primary">
